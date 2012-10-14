b122242e134e21f27af7383d534b31a5f76d36c4
seanoshea <oshea.ie@gmail.com>
2012-10-13 18:22:48 -0700

    Some testing instructions.

--- krissytositest/src/com/krissytosi/HomeActivityTest.java
+++ krissytositest/src/com/krissytosi/HomeActivityTest.java
@@ -1,15 +1,17 @@
+
 package com.krissytosi;
 
 import android.test.ActivityInstrumentationTestCase2;
 
+import com.krissytosi.activities.HomeActivity;
+
 /**
- * This is a simple framework for a test of an Application.  See
- * {@link android.test.ApplicationTestCase ApplicationTestCase} for more information on
- * how to write and extend Application tests.
+ * This is a simple framework for a test of an Application. See
+ * {@link android.test.ApplicationTestCase ApplicationTestCase} for more
+ * information on how to write and extend Application tests.
  * <p/>
- * To run this test, you can type:
- * adb shell am instrument -w \
- * -e class com.krissytosi.HomeActivityTest \
+ * To run this test, you can type: adb shell am instrument -w \ -e class
+ * com.krissytosi.HomeActivityTest \
  * com.krissytosi.tests/android.test.InstrumentationTestRunner
  */
 public class HomeActivityTest extends ActivityInstrumentationTestCase2<HomeActivity> {
@@ -17,5 +19,4 @@ public class HomeActivityTest extends ActivityInstrumentationTestCase2<HomeActiv
     public HomeActivityTest() {
         super("com.krissytosi", HomeActivity.class);
     }
-
 }
